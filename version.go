77337aa9a8f8cd91a9c78abb52e8c8f10a67ca4e
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2022-12-14 01:14:42 +0200

    Updated version and added comment.

--- version.go
+++ version.go
@@ -1,7 +1,10 @@
 package gops
 
+// Don't want to use https://pkg.go.dev/github.com/hashicorp/go-version
+// this is overkill for such small project where only me is maintainer.
+
 // Version gets the version of gops. Maybe do better structure
-// major, minor, manr struct, etc.
+// major, minor, patch.
 func Version() string {
-	return "1.0.10-rc4"
+	return "1.0.10-rc5"
 }
